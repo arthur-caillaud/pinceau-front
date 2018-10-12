c027e60f019de1ebc51f1f7efb6805d46c87bb7b
Arthur Caillaud <arthur@Arthur-Caillaud.local>
2018-10-12 11:48:13 +0200

    Latest version of front

--- app.py
+++ app.py
@@ -3,25 +3,26 @@ import logging
 logging.getLogger('socketIO-client').setLevel(logging.DEBUG)
 logging.basicConfig()
 
-class DrawNamespace(BaseNamespace):
-
-    def on_draw_response(self, *args):
-        print('DRAW', args)
+# class DrawNamespace(BaseNamespace):
+#
+#     def on_draw_response(self, *args):
+#         print('DRAW', args)
 
 class ConnectNamespace(BaseNamespace):
 
     def on_connect_response(self, *args):
         print('CONNECT', args)
 
-    def on_disconnect_response(self, *args):
-        print('DISCONNECT', args)
+    # def on_disconnect_response(self, *args):
+    #     print('DISCONNECT', args)
 
-socketIO = SocketIO('3139b29d.ngrok.io', 80)
+socketIO = SocketIO(host='localhost', port=5000)
 print('Client running...')
 
 connect_namespace = socketIO.define(ConnectNamespace, '/connect')
-draw_namespace = socketIO.define(DrawNamespace, '/draw')
+# draw_namespace = socketIO.define(DrawNamespace, '/draw')
+print ('Namespaces defined.')
 
-connect_namespace.emit('connect')
-draw_namespace.emit('draw')
+connect_namespace.emit('connect', 'I want to connect')
+# draw_namespace.emit('draw', 'square')
 socketIO.wait(seconds=3)
