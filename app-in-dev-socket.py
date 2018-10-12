000f716044c2b393c9cf46fc7cf72475190e832e
Arthur Caillaud <arthur@Arthur-Caillaud.local>
2018-10-12 09:09:49 +0200

    Removing Popol shitier files

--- app-in-dev-socket.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct  9 16:22:44 2018
-
-@author: AntoineFortECP
-"""
-
-###Ceci est le fichier app-in-dev-socket qui doit être inclus dans la branche DEV
--- app-in-dev.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct  9 16:24:52 2018
-
-@author: AntoineFortECP
-"""
-
-###Ceci est le fichier app-in-dev qui doit être inclus dans la branche DEV
