60bbc76702e750c63c9b9e0cb9c41f08c90c7873
Arthur Caillaud <arthurcaillaud@gmail.com>
2018-11-06 10:02:17 +0100

    Getters and setters where useless

--- connect/reception.py
+++ connect/reception.py
@@ -14,29 +14,16 @@ class Reception(Thread):
         self.__cache = ''
     # Methods
     def run(self):
-        is_running = self.is_running()
-        while is_running :
-            connexion, verrou, cache = self.get_connexion(), self.get_verrou(), self.get_cache()
-            message = cache + connexion.recv(1024).decode()
+        while self.__is_running :
+            message = self.__cache + connexion.recv(1024).decode()
             try:
                 shapes = json.loads(message)
-                self.set_cache('')
+                self.__cache = ''
                 with verrou:
                     for shape in shapes:
                         self.draw(shape)
             except:
-                self.set_cache(message)
-    # Getters
-    def get_verrou(self):
-        return self.__verrou
-    def get_connexion(self):
-        return self.__connexion
-    def is_running(self):
-        return self.__is_running
-    def get_cache(self):
-        return self.__cache
+                self.__cache = message
     # Setters
     def set_draw(self, draw_func):
         self.draw = draw_func
-    def set_cache(self, cache):
-        self.__cache = cache
